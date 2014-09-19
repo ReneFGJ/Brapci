932b9a94c8c369dd1077cd09e527996d258d361d
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2014-09-19 17:49:53 -0300

    v0.14.38b

--- bris/index.php
+++ bris/index.php
@@ -2,16 +2,21 @@
 require("cab.php");
 
 echo '<h1>Indicadores<h1>';
-echo '<table width="98%" align="center" class="tabela00" border=0>';
-echo '<TR><TD width="5%"></TD><td rowspan=10 width="20">&nbsp;&nbsp;</td><TD width="90%"></TD>';
+echo '<table width="98%" align="center" class="tabela00" border=0 cellspacing=10>';
+echo '<TR><TD width="5%"></TD><td rowspan=10 width="20">&nbsp;&nbsp;</td>';
 /* Journal */
 echo '<TR valign="top">
 		<TD><img src="img/icone_rk_journal.png"  width="80"></TD>
-		<TD>
+		<TD width="45%">
 			<UL>
-				<LI><A HREF="indicador_jnl_fi.php" class="lt1">Ranking</A></LI>
 				<LI><A HREF="indicador_jnl_fi.php" class="lt1">Fator de Impacto</A></LI>
-				<LI><A HREF="indicador_jnl_h.php" class="lt1">Índice h</A></LI>
+			</UL>
+		<TD><img src="img/icone_rk_autor.png"  width="80"></TD>	
+		<TD width="5%"></TD>
+		<TD width="45%">
+			<UL>
+				<LI><A HREF="indicador_autor_prod.php" class="lt1">Produção</A></LI>
+				<LI><A HREF="indicador_autor_h.php" class="lt1">Índice h</A></LI>
 			</UL>
 		</TD></TR>';	
 
