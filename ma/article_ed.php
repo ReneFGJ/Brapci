84a2b3cb8e0af12ae0ac8bc9502a5207683b562c
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2014-12-01 15:04:59 -0200

    v0.14.45a

---
+++ ma/article_ed.php
@@ -0,0 +1,25 @@
+<?php
+require("cab.php");
+
+echo '<table class="tabela00" border=1 width="100%">';
+echo '<TR valign="top">';
+
+echo '<TD width="*">';
+
+
+echo '<TD width="550" bgcolor="#EFEFEF">';
+ECHO '<select name="dd1"><option value="2">TYPE</option></select>';
+
+echo '<div id="ed_title" class="border1">TITLE</div>';
+echo '</table>';
+
+
+?>
+
+<style>
+	#ed_title 
+		{
+			width: 100%;
+			height: 100px;
+		}
+</style>
