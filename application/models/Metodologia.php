7983c90fc4078816dcdc433c2fee77164f244bbf
Rene FGJr <renefgj@gmail.com>
2015-12-01 08:34:03 -0200

    v0.15.48

--- application/models/Metodologia.php
+++ application/models/Metodologia.php
@@ -1,4 +1,20 @@
 <?php
+// This file is part of the Brapci Software. 
+// 
+// Copyright 2015, UFPR. All rights reserved. You can redistribute it and/or modify
+// Brapci under the terms of the Brapci License as published by UFPR, which
+// restricts commercial use of the Software. 
+// 
+// Brapci is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the ProEthos License for more details. 
+// 
+// You should have received a copy of the Brapci License along with the Brapci
+// Software. If not, see
+// https://github.com/ReneFGJ/Brapci/tree/master//LICENSE.txt 
+/* @author: Rene Faustino Gabriel Junior <renefgj@gmail.com>
+ * @date: 2015-12-01
+ */
 class metodologia extends CI_Model {
 	function le($artigo) {
 		$artigo = strzero($artigo,10);
