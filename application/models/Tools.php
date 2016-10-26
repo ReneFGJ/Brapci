f9af689e24b3d30a1f885e66668798b7f1a4ca26
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2016-10-26 10:54:20 -0200

    v0.16.10.26

--- application/models/Tools.php
+++ application/models/Tools.php
@@ -37,16 +37,42 @@ class tools extends CI_model
 				$txt = troca($txt,',"',';"');
 				return($txt);
 			}
+		function trata($txt)
+			{
+				$txt = troca($txt,'; ',';');
+				for ($r=0;$r < 32;$r++)
+					{
+						if (($r != 13) and ($r != 10))
+							{
+								$txt = troca($txt,chr($r),' ');
+							}		
+					}
+				
+				$t  = array('á','é','í','ó','ú','Á','É','Í','Ó','Ú','ã','õ','Â','Ô','ä','ë','ï','ö','ü','Ä','Ë','Ï','Ö','Ü','â','ê','î','ô','û','Â','Ê','Ô','Û','ç','Ç');
+				$tr = array('a','e','i','o','u','A','E','I','O','U','a','o','A','O','a','e','i','o','u','A','E','I','O','U','a','e','i','o','u','A','E','O','U','c','C');
+				for ($r=0;$r < count($tr);$r++)
+					{
+						$t1 = $t[$r];
+						$t2 = $tr[$r];
+						$txt = troca($txt,$t1,$t2);
+						$txt = troca($txt,utf8_decode($t1),$t2);		
+					}
+				for ($r=128;$r < 255;$r++)
+					{
+						$txt = troca($txt,chr($r),' ');		
+					}
+				$txt = troca($txt,'  ',' ');
+				return($txt);
+			}
 		function gerar_nomes_xls($txt)
 			{
-				$txt = troca($txt,chr(13),';');
-				$txt = troca($txt,chr(10),';');
+				$txt = $this->trata($txt);
 				$lns = splitx(';',$txt);
 				$ns = array();
 				$nf = array();
 				for ($r=0;$r < count($lns);$r++)
 					{
-						$mn = $lns[$r];
+						$mn = trim(UpperCaseSql($lns[$r]));
 						if (isset($ns[$mn]))
 							{
 								$ns[$mn] = $ns[$mn] + 1;
@@ -61,28 +87,41 @@ class tools extends CI_model
 				$i = 0;
 				foreach ($nf as $key => $val) {
 					$i++;
-				    $sx .= strzero($i,4).'. '.$val. chr(13).chr(10);
+				    $sx .= ''.strzero($i,4).';'.$val.''. chr(13).chr(10);
 				}
+				$sx  .= '';
 				return($sx);
 			}
 		function gerar_matriz_xls($txt)
 			{
-				$txt = troca($txt,';','¢');
-				$txt = troca($txt,chr(13),'');
+				$txt = $this->trata($txt);
+				$txt = troca($txt,';','£');
 				$txt = troca($txt,chr(10),';');
+				$txt = troca($txt,chr(13),';');
 				$lns = splitx(';',$txt);
+				
 				$nx = array();
 				$ns = array();
 				$nf = array();
+				
 				for ($r=0;$r < count($lns);$r++)
 					{
 						$mn = $lns[$r];
-						$mn = troca($mn,'¢',';');
+						$mn = troca($mn,'£',';');
 						$au = splitx(';',$mn.';');
 						
 						for ($a=0;$a < count($au);$a++)
 							{
-								$mm = $au[$a];
+								$mm = nbr_autor($au[$a],5);
+								$mm = troca($mm,',','');
+								$mm = troca($mm,'. ','');
+								$mm = troca($mm,'.','');
+								$au[$a] = $mm;								
+							}
+						
+						for ($a=0;$a < count($au);$a++)
+							{
+								$mm = $au[$a];								
 								if (isset($ns[$mm]))
 									{
 										$ns[$mm] = $ns[$mm] + 1;
@@ -90,6 +129,7 @@ class tools extends CI_model
 										$ns[$mm] = 1;
 										array_push($nf,$mm);
 									}
+
 								/* monta matriz */
 								if ($a == 0)
 									{
@@ -105,6 +145,7 @@ class tools extends CI_model
 										for ($b=0;$b < $a;$b++)
 										{
 										$ma = $au[$b];
+																			
 										if (isset($nx[$ma][$mm]))
 											{
 												$nx[$ma][$mm] = $nx[$ma][$mm] + 1;
@@ -118,37 +159,37 @@ class tools extends CI_model
 							}
 						
 					}
-				sort($nf);
-				/*  matriz */
-				$sx = '<table border=1>';
-				$sx .= '<tr><td></td>';
-				foreach ($nf as $key => $val1) {
-					$sx .= '<td>'.$val1.'</td>';
-				}
-				$sx .= '</tr>'.cr();
 				
+				/*  matriz */
+				$sx = '#;';
 				foreach ($nf as $key => $val1) {
-					$sx .= '<tr>';
-					$sx .= '<td>'.$val1.'</td>';
+					$sx .= ''.$val1.';';
+				}
+				$sx .= ''.cr();
+				foreach ($nf as $key => $val1) {
+					$sx .= ''.$val1.';';
 					foreach ($nf as $key2 => $val2 ) {
-						if (isset($nx[$val1][$val2]))
+						if ((isset($nx[$val1][$val2])) AND ($val1 != $val2))
 						{
-				    		$sx .= '<td align="center">'.$nx[$val1][$val2].'</td>';
+				    		$sx .= ''.$nx[$val1][$val2].';';
 						} else {
-							$sx .= '<td align="center">0</td>';
+							$sx .= '0;';
 						}
 					}
+					$sx .= ''.cr();
 				}
-				$sx  .= '</table>';
+				$sx  .= '';
 				return($sx);
 			}
 
 		function gerar_matriz_pajek($txt)
 			{
-				$txt = troca($txt,';','¢');
-				$txt = troca($txt,chr(13),'');
+				$txt = $this->trata($txt);
+				$txt = troca($txt,';','£');
 				$txt = troca($txt,chr(10),';');
+				$txt = troca($txt,chr(13),';');
 				$lns = splitx(';',$txt);
+				
 				$nx = array();
 				$ns = array();
 				$nf = array();
@@ -156,8 +197,17 @@ class tools extends CI_model
 				for ($r=0;$r < count($lns);$r++)
 					{
 						$mn = $lns[$r];
-						$mn = troca($mn,'¢',';');
+						$mn = troca($mn,'£',';');
 						$au = splitx(';',$mn.';');
+						
+						for ($a=0;$a < count($au);$a++)
+							{
+								$mm = nbr_autor($au[$a],5);
+								$mm = troca($mm,',','');
+								$mm = troca($mm,'. ','');
+								$mm = troca($mm,'.','');
+								$au[$a] = $mm;								
+							}						
 						
 						for ($a=0;$a < count($au);$a++)
 							{
@@ -200,9 +250,17 @@ class tools extends CI_model
 				sort($nf);
 				/*  matriz */
 				$sx = '*Vertices '.count($nf).cr();
+				$max = 10;
 				foreach ($nf as $key => $val1) {
-					$n1 = number_format($ns[$val1]/10,4);
-					$sx .= ($key+1).' "'.$val1.'" '.$n1.' '.$ns[$val1].' '.$ns[$val1].' '.cr();
+					if ($ns[$val1] > $max)
+						{
+							$max = $ns[$val1]; 
+						}
+				}
+				foreach ($nf as $key => $val1) {
+					$n1 = number_format($ns[$val1]/$max*10,4);
+					//$sx .= ($key+1).' "'.$val1.'" '.$n1.' '.$ns[$val1].' '.$ns[$val1].' '.cr();
+					$sx .= ($key+1).' "'.$val1.'" ellipse x_fact '.$n1.' y_fact '.$n1.' fos 1 ic LightYellow lc Blue '.cr();
 				}
 
 				$sx .= '*Edges'.cr();
@@ -241,6 +299,7 @@ class tools extends CI_model
 			}
 		function tratar($txt)
 			{
+				$txt = troca($txt,';','¢');
 				$txt = troca($txt,';','.,');
 				$txt = troca($txt,'","','";"');
 				$txt = troca($txt,'"',';');
@@ -249,6 +308,7 @@ class tools extends CI_model
 				$txt = troca($txt,chr(9),'');
 				$txt = troca($txt,chr(15),'');
 				$txt = troca($txt,chr(13),'');
+				$lns = splitx(';',$txt);
 				return($txt);
 			}
 		function export_cols($txt,$col)
