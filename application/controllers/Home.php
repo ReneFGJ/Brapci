6f66f978169690519fedae58814528959bc1e669
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-03-06 16:38:22 -0300

    v0.18.03.06

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,6 +15,7 @@
 /* @author: Rene Faustino Gabriel Junior <renefgj@gmail.com>
  * @date: 2015-12-01
  */
+DEFINE('BASE_PUBLIC',"brapc607_public.");
 
 class home extends CI_Controller {
 	function __construct() {
