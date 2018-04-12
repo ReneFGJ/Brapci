f5f1b9ba4b6786f2f627d7d99a6b4d265a1eea9b
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-04-12 18:10:43 -0300

    v0.18.04.12

--- application/controllers/Tool.php
+++ application/controllers/Tool.php
@@ -37,211 +37,220 @@ class tool extends CI_Controller {
 		/* Model */
 		$this -> load -> model('Search');
 		$this -> load -> view("header/cab");
-        
+
 		/* Mostra rodape */
 		$this -> load -> view("header/foot");
 	}
-    
-    function wordtools()
-        {
-        /* Model */
-        $this -> load -> model('Search');
-        $this -> load -> view("header/cab");
 
-        $form = new form;
-        $cp = array();
-        array_push($cp,array('$H8','','',false,true));
-        array_push($cp,array('$T80:20','','Texto',true,true));
-        array_push($cp,array('$T80:20','','Texto',true,true));
-        array_push($cp,array('$B8','','Processar dados>>>',false,true));
-        $tela = $form->editar($cp,'');
-        
-        if ($form->saved > 0)
-            {
-                $dd1 = get("dd1");
-                $dd2 = get("dd2");
-                $tela .= $this->Search->wcount($dd1,$dd2);
-            }
-        
-        $data['content'] = $tela;
-        $data['title'] = '';
-        $this->load->view('content',$data);
+	function wordtools() {
+		/* Model */
+		$this -> load -> model('Search');
+		$this -> load -> view("header/cab");
 
-        /* Mostra rodape */
-        $this -> load -> view("header/foot");  
-        }
-	
+		$form = new form;
+		$cp = array();
+		array_push($cp, array('$H8', '', '', false, true));
+		array_push($cp, array('$T80:20', '', 'Texto', true, true));
+		array_push($cp, array('$T80:20', '', 'Texto', true, true));
+		array_push($cp, array('$B8', '', 'Processar dados>>>', false, true));
+		$tela = $form -> editar($cp, '');
+
+		if ($form -> saved > 0) {
+			$dd1 = get("dd1");
+			$dd2 = get("dd2");
+			$tela .= $this -> Search -> wcount($dd1, $dd2);
+		}
+
+		$data['content'] = $tela;
+		$data['title'] = '';
+		$this -> load -> view('content', $data);
+
+		/* Mostra rodape */
+		$this -> load -> view("header/foot");
+	}
+
 	function matrix() {
 		/* Model */
 		$this -> load -> model('Search');
-		$this->load->model('tools');
+		$this -> load -> model('tools');
 		$this -> load -> view("header/cab");
-		
-		$tela = $this->tools->file_update();
-		
-		
+
+		$tela = $this -> tools -> file_update();
+
 		/* le arquivo se existe */
-		$tx = $this->tools->readfile();
-		if (strlen($tx) > 0)
-			{
-				$tela .= '<h3>conteúdo</h3>';
-				$tx = $this->tools->tratar($tx);
-				$authors = $this->tools->export_cols($tx,1);
-				$tela .= '<pre>'.$authors.'</pre>';
-				
-			}
-		/* Monta tela */	
+		$tx = $this -> tools -> readfile();
+		if (strlen($tx) > 0) {
+			$tela .= '<h3>conteúdo</h3>';
+			$tx = $this -> tools -> tratar($tx);
+			$authors = $this -> tools -> export_cols($tx, 1);
+			$tela .= '<pre>' . $authors . '</pre>';
+
+		}
+		/* Monta tela */
 		$data['content'] = $tela;
-		$this->load->view('content',$data);
+		$this -> load -> view('content', $data);
 
 		/* Mostra rodape */
 		$this -> load -> view("header/foot");
-	}	
+	}
+
 	function change01() {
 		/* Model */
 		$this -> load -> model('Search');
-		$this->load->model('tools');
+		$this -> load -> model('tools');
 
 		$data['link'] = base_url('index.php/tool/change01');
-		$tela = $this->tools->file_update($data);	
-		
-		/* le arquivo se existe */
-		$tx = $this->tools->readfile();
+		$tela = $this -> tools -> file_update($data);
 
-		if (strlen($tx) > 0)
-			{
-				$tela .= '<h3>conteúdo</h3>';
-				$tx = $this->tools->tratar_xls($tx);
-				$this->tools->download_file($tx);
-				return('');				
-			}
-		/* Monta tela */	
+		/* le arquivo se existe */
+		$tx = $this -> tools -> readfile();
+
+		if (strlen($tx) > 0) {
+			$tela .= '<h3>conteúdo</h3>';
+			$tx = $this -> tools -> tratar_xls($tx);
+			$this -> tools -> download_file($tx);
+			return ('');
+		}
+		/* Monta tela */
 		$this -> load -> view("header/cab");
 		$data['content'] = $tela;
-		$this->load->view('content',$data);
+		$this -> load -> view('content', $data);
 
 		/* Mostra rodape */
 		$this -> load -> view("header/foot");
 	}
+
 	function change02() {
 		/* Model */
 		$this -> load -> model('Search');
-		$this->load->model('tools');
+		$this -> load -> model('tools');
 
 		$data['link'] = base_url('index.php/tool/change02');
-		$tela = $this->tools->file_update($data);	
-		
-		/* le arquivo se existe */
-		$tx = $this->tools->readfile();
+		$tela = $this -> tools -> file_update($data);
 
-		if (strlen($tx) > 0)
-			{
-				$tx = $this->tools->gerar_nomes_xls($tx);
-				$this->tools->download_file($tx);
-				return('');				
-			}
-		/* Monta tela */	
+		/* le arquivo se existe */
+		$tx = $this -> tools -> readfile();
+
+		if (strlen($tx) > 0) {
+			$tx = $this -> tools -> gerar_nomes_xls($tx);
+			$this -> tools -> download_file($tx);
+			return ('');
+		}
+		/* Monta tela */
 		$this -> load -> view("header/cab");
 		$data['content'] = $tela;
-		$this->load->view('content',$data);
+		$this -> load -> view('content', $data);
 
 		/* Mostra rodape */
 		$this -> load -> view("header/foot");
 	}
+
 	function change03() {
 		/* Model */
 		$this -> load -> model('Search');
-		$this->load->model('tools');
+		$this -> load -> model('tools');
 
 		$data['link'] = base_url('index.php/tool/change03');
-		$tela = $this->tools->file_update($data);	
-		
-		/* le arquivo se existe */
-		$tx = $this->tools->readfile();
+		$tela = $this -> tools -> file_update($data);
 
-		if (strlen($tx) > 0)
-			{
-				$tx = $this->tools->gerar_matriz_xls($tx);
-				$this->tools->download_file($tx);
-				return('');				
-			}
-		/* Monta tela */	
+		/* le arquivo se existe */
+		$tx = $this -> tools -> readfile();
+
+		if (strlen($tx) > 0) {
+			$tx = $this -> tools -> gerar_matriz_xls($tx);
+			$this -> tools -> download_file($tx);
+			return ('');
+		}
+		/* Monta tela */
 		$this -> load -> view("header/cab");
 		$data['content'] = $tela;
-		$this->load->view('content',$data);
+		$this -> load -> view('content', $data);
 
 		/* Mostra rodape */
 		$this -> load -> view("header/foot");
 	}
+
 	function change04() {
 		$this -> load -> model('Search');
 		/* Model */
-		$this->load->model('tools');
+		$this -> load -> model('tools');
 
 		$data['link'] = base_url('index.php/tool/change03');
-		$tela = $this->tools->file_update($data);	
-		
-		/* le arquivo se existe */
-		$tx = $this->tools->readfile();
+		$tela = $this -> tools -> file_update($data);
 
-		if (strlen($tx) > 0)
-			{
-				$tx = $this->tools->gerar_matriz_pajek($tx);
-				$this->tools->download_file($tx,'.net');
-				return('');				
-			}
-		/* Monta tela */	
+		/* le arquivo se existe */
+		$tx = $this -> tools -> readfile();
+
+		if (strlen($tx) > 0) {
+			$tx = $this -> tools -> gerar_matriz_pajek($tx);
+			$this -> tools -> download_file($tx, '.net');
+			return ('');
+		}
+		/* Monta tela */
 		$this -> load -> view("header/cab");
 		$data['content'] = $tela;
-		$this->load->view('content',$data);
+		$this -> load -> view('content', $data);
 
 		/* Mostra rodape */
 		$this -> load -> view("header/foot");
-	}	
-    function from_to()
-        {
-        /* Model */
-        $this -> load -> model('Search');
-        $this -> load -> view("header/cab");
+	}
 
-        $form = new form;
-        $cp = array();
-        array_push($cp,array('$H8','','',false,true));
-        array_push($cp,array('$T80:20','','Texto Original',true,true));
-        array_push($cp,array('$T80:20','','Matrix de troca',true,true));
-        array_push($cp,array('$B8','','Processar dados>>>',false,true));
-        $tela = $form->editar($cp,'');
-        
-        if ($form->saved > 0)
-            {
-                $dd1 = get("dd1");
-                $dd2 = get("dd2");
-                
-                $dd2 = troca($dd2,'=>','¢');
-                $dd2 = troca($dd2,';','¢');
-                $dd2 = troca($dd2,'"','');
-                $dd2 = troca($dd2,chr(13),';');
-                $dd2 = troca($dd2,chr(10),'');
-                $words = splitx(';',$dd2);
-                for ($r=0;$r < count($words);$r++)
-                    {
-                        $ln = $words[$r];
-                        $ln = troca($ln,'¢',';');
-                        $ln = splitx(';',$ln);
-                        if (isset($ln[1]))
-                            {
-                                $dd1 = troca($dd1,$ln[0],$ln[1]);
-                            }
-                    }
-                $tela = '<h3>Resultado</h3><textarea rows=15 class="form-control">'.$dd1.'</textarea><br>'.$tela;
-            }
-        
-        $data['content'] = $tela;
-        $data['title'] = '';
-        $this->load->view('content',$data);
+	function from_to() {
+		/* Model */
+		$this -> load -> model('Search');
+		$this -> load -> view("header/cab");
 
-        /* Mostra rodape */
-        $this -> load -> view("header/foot");   
-        }		
+		$form = new form;
+		$cp = array();
+		array_push($cp, array('$H8', '', '', false, true));
+		array_push($cp, array('$T80:20', '', 'Texto Original', true, true));
+		array_push($cp, array('$T80:20', '', 'Matrix de troca', true, true));
+		array_push($cp, array('$B8', '', 'Processar dados>>>', false, true));
+		$tela = $form -> editar($cp, '');
+
+		if ($form -> saved > 0) {
+			$dd1 = get("dd1");
+			$dd2 = get("dd2");
+
+			$dd2 = troca($dd2, '=>', '¢');
+			$dd2 = troca($dd2, ';', '¢');
+			$dd2 = troca($dd2, '"', '');
+			$dd2 = troca($dd2, chr(13), ';');
+			$dd2 = troca($dd2, chr(10), '');
+			$words = splitx(';', $dd2);
+			$wk = array();
+			for ($r = 0; $r < count($words); $r++) {
+				$ln = $words[$r];
+				$ln = troca($ln, '¢', ';');
+				$ln = splitx(';', $ln);
+				if (isset($ln[1])) {
+					$wd = UpperCaseSql($ln[0]);
+					$wk[$wd] = $ln[1];
+				}
+			}
+			/* LINHAS */
+			$lns = troca($dd1, chr(10), '');
+			$lns = troca($lns, chr(13), ';');
+			$lns = splitx(';', $lns);
+			$dd1 = '';
+			for ($n = 0; $n < count($lns); $n++) {
+				$t = $lns[$n];
+				$tn = UpperCaseSql($t);
+				$q = $t;
+				if (isset($wk[$tn])) { $q = $wk[$tn]; }
+				echo '<br>'.$t.'=>'.$q;
+				$dd1 .= $q.chr(13);
+			}
+			$tela = '<h3>Resultado</h3><textarea rows=15 class="form-control">' . $dd1 . '</textarea><br>' . $tela;
+		}
+//exit;
+		$data['content'] = $tela;
+		$data['title'] = '';
+		$this -> load -> view('content', $data);
+
+		/* Mostra rodape */
+		$this -> load -> view("header/foot");
+	}
+
 }
 ?>
